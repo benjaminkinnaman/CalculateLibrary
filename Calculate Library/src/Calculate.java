1d82e12abecdceafe09a366546dcbfe1beecbed9
APCS3rd <APCS3rd@MHS-Dreyer-11>
2018-09-14 10:53:05 -0700

    9/14/2018 just coded foil function

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -3,48 +3,65 @@
 //@version September 6 2018
 
 public class Calculate {
-	public static int square(int number) {
+	public static int square(int number) {   //Squares a value.
 		int answer;
 		answer = number*number;
 		return answer;
 }
 	
-	public static int cube(int number) {
+	public static int cube(int number) {	//Cubes a value.
 		int answer;
 		answer = number * number * number;
 		return answer;
 	}
 	
-	public static double average(double number, double number2) {
+	public static double average(double number, double number2) {	//Averages 2 numbers.
 		double answer;
 		answer = ((number + number2)/2);
 		return answer;
 		
 	}
 	
-	public static double average(double number, double number2, double number3) {	
+	public static double average(double number, double number2, double number3) {	//Averages 3 numbers.
 		double answer;
 		answer = ((number +number2 +number3)/3);
 		return answer;
 	}
 	
-	//public static double toDegrees( ) {
-		
-	//}
+	public static double toDegrees(double radianValue ) {	//Converts an angle measure given in radians to degrees
+		double answer;		
+		answer = radianValue * (180/3.14159);
+		return answer;
+	}
 	
-	//public static double toRadians {
-		
-	//}
+	public static double toRadians(double degreeValue) {		//Converts an angle measure given in degrees to radians
+		double answer;
+		answer = degreeValue * (3.14159/180);
+		return answer;
+	}
 	
 	public static double discriminant(double a, double b, double c) {
 		double answer;
 		answer = ((b*b)-(4*(a*c)));
-		return answer;
-	
+		return answer;	
 	}
 	
-//	public static
-
+//	public static String toImproperFrac(int wholeNumInit, int denom, int initNumerator) {  //Converts a mixed number into an improper fraction.
+//		String answer;
+//		answer = ((wholeNumInit * denom) +  );
+//		return answer;
+//	}
 	
+//	public static double toMixedNum() {		//Converts an improper fraction into a mixed number.
+//		double answer;
+//		answer = ();
+//		return answer;
+//	}
 
+	public static String foil(int a, int b, int c, int d, String var)  {	//Converts a binomial multiplication of the form (ax+b)(cx+d) into a quad equation ax^2+bx+c
+		String answer;
+		answer = (a*c) + var + "^2 + " + ((a*d)+(b*c)) + "n + " + (b*d);
+		return answer;
+	}
+	
 }
